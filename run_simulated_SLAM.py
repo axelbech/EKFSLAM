f9aa7efd2b18b8e28cee7ca5cc546f3ca7894d91
Thomas Andersen <thoander@stud.ntnu.no>
2020-11-06 12:24:59 +0100

    started run_simulated_slam

--- run_simulated_SLAM.py
+++ run_simulated_SLAM.py
@@ -96,14 +96,15 @@ K = len(z)
 M = len(landmarks)
 
 # %% Initilize
-Q = # TODO
-R = # TODO
+Q = np.diag(np.array([1, 1, 1])**2) # TODO
+R = np.eye(2*M) # TODO
+
 
 doAsso = True
 
-JCBBalphas = np.array(
-    # TODO,
-)  # first is for joint compatibility, second is individual
+jointAlpha = 0.05
+individualAlpha = 0.05
+JCBBalphas = np.array([jointAlpha, individualAlpha]) # TODO # first is for joint compatibility, second is individual
 # these can have a large effect on runtime either through the number of landmarks created
 # or by the size of the association search space.
 
@@ -143,10 +144,10 @@ print("starting sim (" + str(N) + " iterations)")
 
 for k, z_k in tqdm(enumerate(z[:N])):
 
-    eta_hat[k], P_hat[k], NIS[k], a[k] = # TODO update
+    eta_hat[k], P_hat[k], NIS[k], a[k] = slam.update(eta_pred[k], P_pred[k], z_k) # TODO update
 
     if k < K - 1:
-        eta_pred[k + 1], P_pred[k + 1] = # TODO predict
+        eta_pred[k + 1], P_pred[k + 1] = slam.predict(eta_hat[k], P_hat[k], odometry[k]) # TODO predict
 
     assert (
         eta_hat[k].shape[0] == P_hat[k].shape[0]
@@ -163,7 +164,7 @@ for k, z_k in tqdm(enumerate(z[:N])):
         NISnorm[k] = 1
         CInorm[k].fill(1)
 
-    NEESes[k] = # TODO, use provided function slam.NEESes
+    NEESes[k] = slam.NEESes(eta_hat[:3,k], P_hat[:3, k], poseGT[k])# TODO, use provided function slam.NEESes
 
     if doAssoPlot and k > 0:
         axAsso.clear()
